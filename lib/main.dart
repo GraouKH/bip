748263cc5994bd2c1ebafb6a784ea403979b062f
GraouKH <cubjulien@gmail.com>
2023-08-16 00:11:48 +0200

    final

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:async';
-import 'dart:ffi';
-
 import 'package:flutter/material.dart';
 import 'package:wakelock/wakelock.dart';
 
@@ -29,12 +27,13 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int seconds = 20;
+  int rep = 0;
   Timer? timer;
-  List<String> myList = List.filled(6, "FD") + List.filled(6, "FC");
+  List<int> reps = List.generate(44, (i) => i % 2 == 0 ? 20 : 10);
+  List<String> exercises = List.filled(6, "HC") + List.filled(6, "3FD") + List.filled(2, "3hC") + List.filled(2, "2HC") + List.filled(2, "2hC") + List.filled(2, "2FD") + List.filled(2, "2fD");
 
   void startTimer() {
-    timer = Timer.periodic(const Duration(seconds: 1), (_) => (seconds > 0) ? setState(() => seconds--) : stopTimer());
+    timer = Timer.periodic(const Duration(seconds: 1), (_) => (reps[rep] > 0) ? setState(() => reps[rep]--) : setState(() => (rep < 45) ? rep++ : resetTimer()));
     Wakelock.enable();
   }
 
@@ -44,28 +43,35 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   void resetTimer() {
-    setState(() => seconds = 20);
+    setState(() {
+      reps = List.generate(44, (i) => i % 2 == 0 ? 20 : 10);
+      rep = 0;
+    });
     stopTimer();
   }
 
-  void plus5() => setState(() => (seconds < 15) ? seconds += 5 : seconds = 20);
-  void moins5() => setState(() => (seconds > 5) ? seconds -= 5 : seconds = 0);
-
+  void plus5() => setState(() => (reps[rep] < 15) ? reps[rep] += 5 : reps[rep] = 20);
+  void moins5() => setState(() => (reps[rep] > 5) ? reps[rep] -= 5 : reps[rep] = 0);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: (rep % 2 == 0) ? Colors.white : Colors.black,
       body: GestureDetector(
         behavior: HitTestBehavior.opaque,
         onTap: () => (timer == null || !timer!.isActive) ? startTimer() : stopTimer(),
         onDoubleTapDown: (d) => (d.globalPosition.dx > MediaQuery.of(context).size.width / 2) ? plus5() : moins5(),
         onLongPress: () => resetTimer(),
         child: Center(
-          child: Text(
-            '$seconds',
-            style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: Colors.black),
-          ),
+          child: (rep % 2 == 0) ? 
+            Text(
+              '${exercises[rep]}\n${reps[rep]}',
+              style: const TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: Colors.black),
+            ) : 
+            Text(
+              '${reps[rep]}',
+              style: TextStyle(fontSize: 80, fontWeight: FontWeight.bold, color: (rep % 2 == 0) ? Colors.black : Colors.white),
+            ),
         ),
       ),
     );
